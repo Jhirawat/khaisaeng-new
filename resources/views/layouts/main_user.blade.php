dc523bb15f58176a8d3c16f5799bbe4a0c575f84
Rathapon <rathapon06a@gmail.com>
2023-03-10 14:41:33 +0700

    main user

--- resources/views/layouts/main_user.blade.php
+++ resources/views/layouts/main_user.blade.php
@@ -492,76 +492,39 @@
                 {{-- <li class=" navigation-header"><span>Dashboard</span>
                 </li> --}}
                 <li class="{{ Request::routeIs('products.list') ? 'active' : '' }} nav-item"><a
-                        href="{{ route('products.list') }}"><i class="bi bi-house-door-fill"
+                        href="{{ route('products') }}"><i class="bi bi-house-door-fill"
                             style="margin-top: 3px;"></i><span class="menu-title" data-i18n="Email"
                             style="font-size: 18px">หน้าหลัก</span></a>
                 </li>
                 <li class="{{ Request::routeIs('') ? 'active' : '' }} nav-item"><a
-                    href="{{ route('products.list') }}"><i class="fa fa-shopping-basket"
+                    href="{{ route('products') }}"><i class="fa fa-shopping-basket"
                         style="margin-top: 3px;"></i><span class="menu-title" data-i18n="Email"
-                        style="font-size: 18px">รายการสั่งซื้อ</span></a>
+                        style="font-size: 18px">ตะกร้าสินค้า</span></a>
+            </li>
+            <li class="{{ Request::routeIs('') ? 'active' : '' }} nav-item"><a
+                    href="{{ route('products') }}"><i class="fa fa-map-marker"
+                        style="margin-top: 3px;"></i><span class="menu-title" data-i18n="Email"
+                        style="font-size: 18px">ที่อยู่</span></a>
             </li>
 
-                <li class="{{ Request::routeIs('stock') ? 'active' : '' }} nav-item">
-                    <a href=""><i class="bi bi-box-seam"style="margin-top: 3px;"></i><span class="menu-title"
-                            data-i18n="Todo"style="font-size: 18px; ">ประเภทสินค้า</span></a>
-                    <ul class="menu-content">
-                        <li class="{{ Request::routeIs('stock') ? 'active' : '' }} nav-item"><a href=""><i
-                                    class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                                    class="menu-item" data-i18n="Shop"></span></a>
-                        </li>
-                        <li class="{{ Request::routeIs('addInventory') ? 'active' : '' }} nav-item"><a
-                                href=""><i class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                                    class="menu-item" data-i18n="Shop">เพิ่มสินค้า</span></a>
-                        </li>
-
-                    </ul>
-                </li>
+        
                 <li class="{{ Request::routeIs('') ? 'active' : '' }} nav-item">
-                    <a href=""><i class="fa fa-line-chart"style="margin-top: 3px;"></i><span
+                    <a href=""><i class="fa fa-check-square-o"style="margin-top: 3px;"></i><span
                             class="menu-title" data-i18n="Calender"style="font-size: 18px; ">ประวัตืการสั้งซื้อ
                         </span></a>
                     <ul class="menu-content">
                         <li class="{{ Request::routeIs('employee') ? 'active' : '' }} nav-item"><a href=""><i
                                     class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                                    class="menu-item">รายการล่าสุด</span></a>
+                                    class="menu-item">ประวัตืการสั้งซื้อ</span></a>
                         </li>
                         <li class="{{ Request::routeIs('addEmployee1') ? 'active' : '' }} nav-item"><a
                                 href=""><i class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                                    class="menu-item">ฟฟฟฟฟฟฟฟฟ</span></a>
+                                    class="menu-item">ใบเสร็จ</span></a>
                         </li>
-                        {{-- <li class="{{ Request::routeIs('employeesetting') ? 'active' : '' }} nav-item"><a
-                                href="{{ route('employeesetting') }}"><i class="feather icon-circle"
-                                    style="font-size: 1.2rem;"></i><span class="menu-item"
-                                    data-i18n="Shop">รายชื่อพนักงานลาออก</span></a>
-                        </li> --}}
                     </ul>
                 </li>
-                <li class="{{ Request::routeIs('departsetting') ? 'active' : '' }} nav-item"><a href=""><i
-                            class="fa fa-gear"style="margin-top: 3px;"></i><span class="menu-title"
-                            data-i18n="Todo"style="font-size: 18px; ">ตั้งค่า</span></a>
-                    <ul class="menu-content">
-                        <li class="{{ Request::routeIs('departsetting') ? 'active' : '' }} nav-item"><a
-                                href=""><i class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                                    class="menu-item" data-i18n="Details">ประเภทสินค้า</span></a>
-                        </li>
 
-                        <li class="{{ Request::routeIs('tax') ? 'active' : '' }} nav-item"><a href=""><i
-                                    class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                                    class="menu-item" data-i18n="Checkout">หน่วยนับสินค้า
-                                </span></a>
-                        </li>
-                        <li class="{{ Request::routeIs('tax') ? 'active' : '' }} nav-item"><a href=""><i
-                            class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                            class="menu-item" data-i18n="Checkout">ที่ตั้งวิศหกิจ
-                        </span></a>
-                        <li class="{{ Request::routeIs('tax') ? 'active' : '' }} nav-item"><a href=""><i
-                            class="feather icon-circle" style="font-size: 1.2rem;"></i><span
-                            class="menu-item" data-i18n="Checkout">ใบเสร็จ
-                        </span></a>
-                </li>
-                    </ul>
-                </li>
+              
                 <li class="{{ Request::routeIs('logout') ? 'active' : '' }} nav-item"><a href=""
                         onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();"><i
@@ -685,4 +648,3 @@
 <!-- END: Body-->
 
 </html>
-
